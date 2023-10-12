58c39e1cd8a8befced5f25bc21a98e398f1ba367
saintpreston <90055250+Saintpreston@users.noreply.github.com>
2023-10-11 20:26:32 -0400

    "_"

--- src/components/ControlPanel/index.tsx
+++ src/components/ControlPanel/index.tsx
@@ -13,14 +13,14 @@ export function ControlPanel({ simulator }: ControlPanelProps) {
   const handleAddObject: MouseEventHandler<HTMLButtonElement> = () => {
     simulator.addObject();
   };
-  
+
   const closedClass = isOpen ? "" : " " + s.closed;
 
   return (
     <aside className={s.ControlPanelContainer}>
       <div className={s.ControlPanel + closedClass}>
-        <button onClick={() => setIsOpen(!isOpen)}>X</button>
-        <h3>Control Panel {JSON.stringify(isOpen)}</h3>
+        <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'X' : 'Open'}</button>
+        <h3>Control Panel</h3>
         <button onClick={handleAddObject}>Add Object</button>
       </div>
     </aside>
